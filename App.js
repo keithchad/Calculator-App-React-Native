2106eb51b1e00a6b815820b0e93e6e91052d29dc
keithchad <keithchad10@outlook.com>
2020-11-21 10:36:45 +0300

    basic layout

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, StatusBar } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
 
 export default class App extends Component {
 
@@ -11,7 +11,42 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World</Text>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>11*11</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>121</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>
+            <View style={styles.row}>
+              <Button title='0' />
+              <Button title='0' />
+              <Button title='0' />
+            </View>
+            <View style={styles.row}>
+              <Button title='0' />
+              <Button title='0' />
+              <Button title='0' />
+            </View>
+            <View style={styles.row}>
+              <Button title='0' />
+              <Button title='0' />
+              <Button title='0' />
+            </View>
+            <View style={styles.row}>
+              <Button title='0' />
+              <Button title='0' />
+              <Button title='0' />
+            </View>
+          </View>
+          <View style={styles.operations}>
+            <Button title='-' />
+            <Button title='-' />
+            <Button title='-' />
+            <Button title='-' />
+          </View>
+        </View>
       </View>
     );
   }
@@ -19,7 +54,47 @@ export default class App extends Component {
 };
 
 const styles = StyleSheet.create({
-    container : {
-      color: 'red',
-    }
+  container: {
+    flex: 1.0,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1.0,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  result: {
+    flex: 2.0,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  resultText : {
+    fontSize: 30.0,
+    color: 'white'
+  },
+  calculation: {
+    flex: 1.0,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  calculationText : {
+    fontSize: 24.0,
+    color: 'white'
+  },
+  buttons: {
+    flexGrow: 7.0,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 3.0,
+    backgroundColor: 'yellow',
+  },
+  operations: {
+    flex: 1.0,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    backgroundColor: 'black'
+  }
 });
